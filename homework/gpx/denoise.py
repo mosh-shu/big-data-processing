6c937917a345792429f3f9f1b47e4cc3d0d52563
Shu Sakamoto <shusakamoto1707@gmail.com>
2019-07-09 09:13:32 +0900

    denoise

--- homework/gpx/denoise.py
+++ homework/gpx/denoise.py
@@ -1,18 +1,25 @@
-import csv, sys
+import csv
+import sys
+
+path = sys.argv[1]
 
 file = path + "all.csv"
 outs = list()
 
-csv_file = open(file, "r", encoding="udf_8")
-f = csv.reader(csv_file, delimiter=",",lineterminator="\r\n", quotechar='"', \
-                skipinitialspace=True)
+csv_file = open(file, "r", encoding="utf_8")
+f = csv.reader(csv_file, delimiter=",", lineterminator="\r\n", quotechar='"',
+               skipinitialspace=True)
 
 for row in f:
-    if row[0][2] > 200:
+    if float(row[1]) > 200:
+        print(row)
+        continue
+    if float(row[0]) > 35.7:
+        print(row)
         continue
     out = row
     outs.append(out)
 
 with open(file, 'w') as ff:
-    writer = csv.writer(ff, lineterminator='\n') # 改行コード（\n）を指定しておく
-    writer.writerows(outs) # 2次元配列も書き込める
+    writer = csv.writer(ff, lineterminator='\n')  # 改行コード（\n）を指定しておく
+    writer.writerows(outs)  # 2次元配列も書き込める
