03e69d82d3daaf1b1241a03ad705c47c693b6998
Shu@gpgpu02 <shusakamoto1707@gmail.com>
2019-07-13 21:24:00 +0900

    I hope this is okay

--- homework/gpx/gpx.R
+++ homework/gpx/gpx.R
@@ -1,4 +1,14 @@
-# After python convertTracking.py
-
-track <- read.csv("nagoya/TrackingData/all/.csv", header=F)
+track <- read.csv("nagoya/TrackingSpeedData/all_denoise.csv", header=F)
+png("report/images/allplot_denoise.png", width = 700, height = 700)  # 描画デバイスを開く
 plot(track[,2], track[,1])
+dev.off()
+
+library(ggmap)
+library(ggplot2)
+register_google(key="*")
+map <- get_map(location=c(min(track[,2]), min(track[,1]), max(track[,2]), max(track[,1])),source='google')
+ggmap(map)
+track[,3] <- as.POSIXct(track[,3], "%Y-%m-%dT%H:%M:%S", tz="Japan")
+ggmap(map) + geom_point(data=track, aes(track[,2], track[,1]), color='red')
+MissCol <- miss_cat(track, var1 = colnames(track[1]), var2 = colnames(track[2]))
+
